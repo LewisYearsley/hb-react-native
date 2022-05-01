6e5db671a0c982f31438f61840fadbcffdb59438
saied.nawaz <saied.nawaz@infinityworks.com>
2022-05-01 13:22:51 +0100

    Configure Port

---
+++ server/README.md
@@ -0,0 +1,20 @@
+# HB React Test server
+
+## Setting up
+
+```bash
+yarn install
+```
+
+## Env
+
+### Port
+
+Default port is set to `4000`
+To configure your port number please copy `.env.example` to `.env` and change the port number.
+
+## Running the app
+
+```bash
+yarn start
+```
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,10 @@ import middleware from './middleware'
 
 import logger from './util/logger'
 
-const PORT = 4000 // TODO: make this configurable
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const PORT = process.env.PORT || 4000
 
 function createServer(): express.Application {
   const server = express()
